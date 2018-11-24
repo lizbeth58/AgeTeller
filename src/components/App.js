989d4cb4d3100407cc40e47e523096a541bfca65
lizbeth58 <ortiz.beth58@gmail.com>
2018-11-23 23:21:14 -0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
+import "./App.css";
+import AgeStats from "./AgeStats";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: "",
+      birthday: "1992-06-21",
+      showStats: false
+    };
+  }
+
+  changeBirthday() {
+    this.setState({ birthday: this.state.newDate, showStats: true });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Form inline>
+          <h2>Input Your Birthday!</h2>
+          <FormControl
+            type='date'
+            onChange={event => this.setState({ newDate: event.target.value })}
+          />{" "}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
+          {this.state.showStats ? (
+            <div className='fade age-stats'>
+              <AgeStats date={this.state.birthday} />
+            </div>
+          ) : null}
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default App;
